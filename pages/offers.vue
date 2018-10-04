9d1d05d7cb73de95f66015d8b51e61f00e0ed99c
elenamlopez <elena.mateos1@gmail.com>
2018-10-04 21:23:11 +0200

    Added a new page, or component

---
+++ pages/offers.vue
@@ -0,0 +1,18 @@
+<template>
+  <main class="offers-container">
+    <h1> Titulo de oferta</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur dolor, similique laudantium recusandae asperiores aspernatur sequi eum, rerum eaque, minus atque. Blanditiis quibusdam voluptatum nulla voluptate quaerat recusandae iure totam!
+  </p>
+  </main>
+</template>
+
+<style scoped>
+  .offers-container {
+    border: 1px solid #aaa;
+    margin: 20px auto;
+    padding: 8px;
+    text-align: left;
+    width: 80%;
+  }
+</style>
+
